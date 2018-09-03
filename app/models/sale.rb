416d0a89c0556bc750e00412f67ac88df99337c5
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-02 23:08:01 -0300

    Add validations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,10 @@
 class Sale < ApplicationRecord
+  validates :cod, uniqueness: true
+  validates :detail, presence: true
+  validates :category, inclusion: %w(uno dos tres cuatro cinco)
+  validates :value, numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 0 }
+  validates :discount, numericality: { only_integer: true,
+                                       greater_than_or_equal_to: 0,
+                                       less_than_or_equal_to: 40 }
 end
