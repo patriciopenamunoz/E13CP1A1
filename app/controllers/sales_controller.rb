1356270bde81e4edd764656b84cac9c5b2617bc9
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-03 19:56:22 -0300

    Fix total calc

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,13 +6,11 @@ class SalesController < ApplicationController
   def create
     @sale = Sale.new(sale_params)
 
-    @sale.tax   = params[:sale][:tax_exempt] == 1 ? 19 : 0
+    @sale.tax   = params[:sale][:tax_exempt].to_i.zero? ? 19 : 0
     @sale.total = @sale.value
     @sale.discount = @sale.discount.to_i
-    @sale.total = @sale.discount > 0 ?
-                  @sale.total * ((@sale.discount/100)+1) : @sale.total
-    @sale.total = @sale.tax > 0 ?
-                  @sale.total * ((@sale.tax/100)+1)      : @sale.total
+    @sale.total = apply_discount @sale.total, @sale.discount
+    @sale.total = apply_discount @sale.total, @sale.tax, 1
 
     @sale.save
     redirect_to sales_done_path
@@ -20,6 +18,20 @@ class SalesController < ApplicationController
 
   def done
     @sales = Sale.all
+  end
+
+  private
+
+  def apply_discount(value, discount, tax = 0)
+    if discount.zero?
+      value
+    else
+      if tax == 1
+        value + (value * ((discount.to_f / 100)))
+      else
+        value - (value * ((discount.to_f / 100)))
+      end
+    end
   end
 
   def sale_params
