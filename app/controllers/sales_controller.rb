10556e0e34bea99aac72b3221df5c92a3f48fcab
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-02 23:58:26 -0300

    Add functionality to create a sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -4,5 +4,21 @@ class SalesController < ApplicationController
   end
 
   def create
+    @sale = Sale.new(sale_params)
+
+    @sale.tax   = params[:sale][:tax_exempt] == 1 ? 19 : 0
+    @sale.total = @sale.value
+    @sale.discount = @sale.discount.to_i
+    @sale.total = @sale.discount > 0 ?
+                  @sale.total * ((@sale.discount/100)+1) : @sale.total
+    @sale.total = @sale.tax > 0 ?
+                  @sale.total * ((@sale.tax/100)+1)      : @sale.total
+
+    @sale.save
+  end
+
+  def sale_params
+    params.require(:sale).permit(:cod, :detail, :category, :value,
+                                 :discount, :tax, :total)
   end
 end
